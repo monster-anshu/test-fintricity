762717634585b215c621c3bde8529cb13856342c
monster-anshu <hgunwant2312@gmail.com>
2022-01-24 10:17:56 +0530

    Update

--- src/Component/Row.jsx
+++ src/Component/Row.jsx
@@ -2,17 +2,12 @@ import React from "react";
 
 function Row(props) {
   const { item } = props;
-  console.log(item);
+  const value = Object.values(item);
   return (
     <tr>
-      <td>{item.pollutant}</td>
-      <td>{item.sector}</td>
-      <td>{item.source}</td>
-      <td>{item.fuel_name}</td>
-      <td>{item.year}</td>
-      <td> {item.emission_factor} </td>
-      <td> {item.unit} </td>
-      <td> {item.activity_unit} </td>
+      {value.map((element) => {
+        return <td key={value.indexOf(element)} >{element}</td>;
+      })}
     </tr>
   );
 }
